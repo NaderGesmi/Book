94d6b350f837a026cd6f395c5cbd857aa6e70bc8
NaderGesmi <nedergesmi35@gmail.com>
2022-03-12 13:39:51 +0100

    d

---
+++ src/components/data.js
@@ -0,0 +1,5 @@
+export const bookData=[
+    { id: 1, name: 'one piece', author: 'eichiro odaa', categorie: 'categorie1'},
+    { id: 2, name: 'naruto', author: 'ùmpohamed', categorie: 'categorie2'},
+    { id: 3, name: 'death note', author: 'imed', categorie: 'categorie3'}
+]
